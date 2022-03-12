4f13feac04f4d872370f8b1127d4fd9a6707933f
younme20 <eunkyeong_b@naver.com>
2022-03-13 07:47:12 +0900

    ADD : 거스름돈 동전 개수 구하기 See also : 문제 해결중

--- src/Chapter05Array.java
+++ src/Chapter05Array.java
@@ -98,6 +98,53 @@ public class Chapter05Array {
         }
 
 
+        //[5-7]
+        //System.out.println("args "+args);
+//        if(args.length!=1) {
+//            System.out.println("USAGE: java Exercise5_7 3120");
+//            System.exit(0);
+//        }
+        // . . 문자열을 숫자로 변환한다 입력한 값이 숫자가 아닐 경우 예외가 발생한다
+        //int money2 = Integer.parseInt(args[0]);
+        int money2 = 3000;
+        System.out.println("money="+money2);
+        int[] coinUnit2 = {500, 100, 50, 10 }; // 동전의 단위
+        int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수
+        for(int i=0;i<coinUnit2.length;i++) {
+            int coinNum = 0;
+        /* (1) . 아래의 로직에 맞게 코드를 작성하시오
+        1. (money) (coinNum) . 금액 을 동전단위로 나눠서 필요한 동전의 개수 를 구한다
+        2. coin coinNum . 배열 에서 만큼의 동전을 뺀다
+        ( coin .) 만일 충분한 동전이 없다면 배열 에 있는 만큼만 뺀다
+        3. (coinNum) . 금액에서 동전의 개수 와 동전단위를 곱한 값을 뺀다
+        */
+            coinNum = money2 / coinUnit2[i];
+            int temp = 0;
+            //coin[i] = coinNum > coin[i] ? 0 : coin[i]-coinNum;
+            if(coinNum > coin[i]) {
+                temp = coin[i];
+                coin[i] = 0;
+            }else {
+                System.out.println(coin[i] + " ::" + coinNum);
+                temp = coin[i] - coinNum;
+                coin[i] = temp;
+            }
+            money2 -= coinUnit2[i]*temp;
+            System.out.println(money2+" : "+temp);
+
+            //System.out.println(coinUnit2[i]+"원 : "+coinNum);
+        }
+        if(money2 > 0) {
+            System.out.println("거스름돈이 부족합니다");
+            System.exit(0); // . 프로그램을 종료한다
+        }
+        System.out.println("=남은 동전의 개수=");
+        for(int i=0;i<coinUnit2.length;i++) {
+            System.out.println(coinUnit2[i]+"원 :"+coin[i]);
+        }
+
+
+
 
 
 
